d4008071454816fe7d26ca369fcf461618e859dc
michalwojewoda <mw.pryw@gmail.com>
2021-01-08 23:17:24 +0100

    admin panel check

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     #own apps
 
     "product",
-    "profil",
     "pages",
 ]
 
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view, contact_view
+from pages.views import home_view, contact_view, blog_view
 
 urlpatterns = [
 
     path('', home_view, name='home'),
     path('contact/', contact_view),
+    path('blog/', blog_view),
     path('admin/', admin.site.urls),
 
 ]
